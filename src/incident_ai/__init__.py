9ce899897153f4d545ee3c1c0d5cafa37f33b911
Edward Tillmann <eddy458@gmx.de>
2026-08-20 18:51:31 +0200

    Bump runtime version to 0.11.0

--- src/incident_ai/__init__.py
+++ src/incident_ai/__init__.py
@@ -4,4 +4,4 @@ from .analyzer import analyze_all, analyze_text
 from .models import IncidentAnalysis
 
 __all__ = ["IncidentAnalysis", "analyze_all", "analyze_text"]
-__version__ = "0.10.2"
+__version__ = "0.11.0"
